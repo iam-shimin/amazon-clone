0a9faa5b67f4440d64e81424a91546a68b143210
shimin mathew <simplyshimin@gmail.com>
2020-09-11 11:45:34 +0530

    Updates

---
+++ precache-manifest.160e8d6eb4c21d485dc64bce9ac3f6b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f49351cb6bb1a9e8d355c8d31e732e",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "68f819141cf29082ef8a",
+    "url": "/amazon-clone/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "792fee5172162eebd94a",
+    "url": "/amazon-clone/static/js/2.1f6a0a2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/amazon-clone/static/js/2.1f6a0a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f819141cf29082ef8a",
+    "url": "/amazon-clone/static/js/main.092d7573.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
